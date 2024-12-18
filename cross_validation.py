9d33f4f0c0f9ab8ebe68f2850df9df75e989c509
Wilhassist <bounsiwilhelmine@gmail.com>
2024-12-18 16:25:21 +0100

    "."

--- cross_validation.py
+++ cross_validation.py
@@ -60,10 +60,10 @@ for train_index, val_index in skf.split(X, y):
     print(columns_with_nan)
 
     # Check rows with NaN values in each column
-for col in X_train_cv.columns:
-    if X_train_cv[col].isna().sum() > 0:
-        print(f"Rows with NaN values in column {col}:")
-        print(X_train_cv[X_train_cv[col].isna()])
+    for col in X_train_cv.columns:
+        if X_train_cv[col].isna().sum() > 0:
+            print(f"Rows with NaN values in column {col}:")
+            print(X_train_cv[X_train_cv[col].isna()])
 
     # Scale the data
     scaler = StandardScaler()
